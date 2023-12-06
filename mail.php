9e9982cf60ba476bee251c73ea2561db599a5792
sangmalik <90916281+sangmalik@users.noreply.github.com>
2023-12-06 20:48:51 +0545

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,18 +1,19 @@
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $to = "sangampaudel530@gmail.com";  // Replace with your email address
+    // You can customize the email content and recipient
+    $to = "your.email@example.com";
     $subject = "New Contact Form Submission";
     $headers = "From: $email";
 
+    // Send the email
     mail($to, $subject, $message, $headers);
-    
-    // You can add additional logic or redirects here after processing the form
-    
-    header("Location: thank-you.html");  // Redirect to a thank-you page
+
+    // Redirect to a thank-you page or display a success message
+    header("Location: thank.html");
     exit();
 }
-?>
